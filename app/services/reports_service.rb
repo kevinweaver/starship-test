51defea01267894669192fd22bace4cd778b0803
Kevin Weaver <kevinweaver2@gmail.com>
2019-10-03 16:36:12 -0400

    Return rows

--- app/services/reports_service.rb
+++ app/services/reports_service.rb
@@ -2,18 +2,18 @@ class ReportsService
 
   def self.overlap_report
     sql = %{
-    SELECT roster1.id, roster1.start_date, roster2.end_date, roster2.id, roster2.start_date, roster2.end_date
-    FROM starship_rosters as roster1, starship_rosters as roster2
-    WHERE exists (SELECT 1
-                  FROM  starship_rosters as roster2
-                  WHERE roster1.start_date BETWEEN roster1.start_date AND roster1.end_date
-			            OR    roster2.end_date BETWEEN roster1.start_date AND roster1.end_date
-			            OR    roster1.start_date BETWEEN roster2.start_date AND roster2.end_date
-			            OR    roster1.end_date BETWEEN roster2.start_date AND roster2.end_date
-                  AND   roster2.id != roster1.id);
+      SELECT roster1.starship_id, roster1.id, roster1.start_date, roster2.end_date, roster2.id, roster2.start_date, roster2.end_date
+      FROM starship_rosters as roster1, starship_rosters as roster2
+      WHERE exists (SELECT 1
+                    FROM  starship_rosters as roster2
+                    WHERE roster1.start_date BETWEEN roster1.start_date AND roster1.end_date
+		  	            OR    roster2.end_date BETWEEN roster1.start_date AND roster1.end_date
+		  	            OR    roster1.start_date BETWEEN roster2.start_date AND roster2.end_date
+		  	            OR    roster1.end_date BETWEEN roster2.start_date AND roster2.end_date
+                    AND   roster2.id != roster1.id);
     }
 
-    execute(sql)
+    execute(sql).rows
   end
 
   private
